9158126fd5330a64fab238ad53aed49281ff33e3
Hichem Kedjour <hichem.kedjour@broadsign.com>
2023-06-08 17:25:53 +0200

    Removed Gtk dependency

--- mcs/class/System.Windows.Forms/System.Windows.Forms.VisualStyles/VisualStylesEngine.cs
+++ mcs/class/System.Windows.Forms/System.Windows.Forms.VisualStyles/VisualStylesEngine.cs
@@ -39,12 +39,7 @@ namespace System.Windows.Forms.VisualStyles
 			string environment_variable = Environment.GetEnvironmentVariable("MONO_VISUAL_STYLES");
 			if (environment_variable != null)
 				environment_variable = environment_variable.ToLower ();
-			if (
-#if !VISUAL_STYLES_USE_GTKPLUS_ON_WINDOWS
-				environment_variable == "gtkplus" &&
-#endif
-				VisualStylesGtkPlus.Initialize ())
-				return new VisualStylesGtkPlus ();
+
 			return new VisualStylesNative ();
 		}
 	}
